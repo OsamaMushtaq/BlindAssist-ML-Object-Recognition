bf8bdb8a633576fd2bb23581c60f176a30b2284e
Usama Mushtaq <126874228+OsamaMushtaq@users.noreply.github.com>
2026-01-06 18:28:28 +0500

    Create config.py

---
+++ src/utils/config.py
@@ -0,0 +1,13 @@
+"""
+Configuration file for BlindAssist
+"""
+
+# GPIO Pins
+TRIG_PIN = 23
+ECHO_PIN = 24
+
+# Thresholds
+OBSTACLE_DISTANCE_THRESHOLD = 100  # cm
+
+# Emergency
+EMERGENCY_CONTACT = "+92XXXXXXXXXX"
