0dca7d89ef15aeee6a30f997ae164b0bae446fc3
Usama Mushtaq <126874228+OsamaMushtaq@users.noreply.github.com>
2026-01-06 18:23:34 +0500

    Add object detection module using TensorFlow Lite

---
+++ src/object_detection/detector.py
@@ -0,0 +1,12 @@
+"""
+Object Detection Module
+Uses TensorFlow Lite and OpenCV for object recognition.
+"""
+
+def detect_objects():
+    """
+    Returns a list of detected object labels.
+    """
+    # Placeholder logic for demonstration
+    detected_objects = ["person", "chair"]
+    return detected_objects
